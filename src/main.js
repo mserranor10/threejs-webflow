be4d635aad75b7d7a98b0bb8b3e454fc9462a8fe
mserranor10 <160638094+mserranor10@users.noreply.github.com>
2024-02-21 19:05:58 +0100

    session 5

--- src/main.js
+++ src/main.js
@@ -3,30 +3,40 @@ import { WaterEffect } from './WaterEffect'
 import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
 import { FontLoader } from 'three/addons/loaders/FontLoader.js';
 
+
+
+var container = document.getElementById( 'webgl' );
+
 const renderer = new THREE.WebGLRenderer({ 
 	alpha: true,
-	});
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.append( renderer.domElement );
+	antialias: true
+});
+renderer.setSize( container.clientWidth-16, container.clientHeight-16 );
+renderer.setAnimationLoop( animationLoop );
+container.appendChild( renderer.domElement );
+
+
+//renderer.setSize( window.innerWidth, window.innerHeight);
+//document.body.appendChild( renderer.domElement );
 
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-camera.position.z = 275;
+const camera = new THREE.PerspectiveCamera( 100, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.z = 100;
 
 
 const loader = new FontLoader();
 
-loader.load( '/satoshi.json', function ( font ) {
+loader.load( 'https://uploads-ssl.webflow.com/65d48c89e255b4e76fcc9054/65d624164654f4a99883bb40_satoshi.txt', function ( font ) {
 
-	const geometry = new TextGeometry( 'Hello three.js!', {
+	const geometry = new TextGeometry( 'Name 1', {
 		font: font,
-		size: 80,
+		size: 25,
 		height: 0
 	} );
 
 	//const geometry = new THREE.BoxGeometry()
-	const material = new THREE.MeshNormalMaterial()
+	const material = new THREE.MeshMatcapMaterial({ color: 0x333 });
 	const mesh = new THREE.Mesh(geometry, material)
 	scene.add(mesh)
 
